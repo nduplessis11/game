51997a5b179a5b027a092bb74744d01791e88884
Nick <nduplessis11@gmail.com>
2023-09-10 15:09:30 -0400

    Remove tabs

--- game/main.h
+++ game/main.h
@@ -8,22 +8,22 @@
 
 typedef struct GameBitmap
 {
-	BITMAPINFO bitmap_info;
-	void* memory;
+    BITMAPINFO bitmap_info;
+    void* memory;
 } GameBitmap;
 
 typedef struct Pixel32
 {
-	uint8_t blue;
-	uint8_t green;
-	uint8_t red;
-	uint8_t alpha;
+    uint8_t blue;
+    uint8_t green;
+    uint8_t red;
+    uint8_t alpha;
 } Pixel32;
 
 LRESULT CALLBACK main_window_procedure(_In_ HWND window,
-									   _In_ UINT message,
-									   _In_ WPARAM w_param,
-									   _In_ LPARAM l_param);
+                                       _In_ UINT message,
+                                       _In_ WPARAM w_param,
+                                       _In_ LPARAM l_param);
 DWORD create_game_window(_In_ HINSTANCE instance);
 BOOL another_instance_is_active(void);
 void process_player_input(void);
